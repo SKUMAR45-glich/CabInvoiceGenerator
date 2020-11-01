ac2cc955c8df37865986eaf45f2e0c7aa92a3e9f
Saurabh <saurabh.nit20@gmail.com>
2020-11-02 00:51:23 +0530

    [Saurabh].Calculate Fare for Normal Ride

---
+++ CanInvoiceTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using CabInvoiceGenerator;
+namespace CanInvoiceTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+    }
+}
