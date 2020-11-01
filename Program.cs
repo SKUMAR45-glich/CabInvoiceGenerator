953f68a24750f2c0ced57a71a0b35f4e3b3b8aea
Saurabh <saurabh.nit20@gmail.com>
2020-11-02 00:43:51 +0530

    [Saurabh].Calculate Fare for Normal Ride

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace CabInvoiceGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome To Cab Invoice Generator");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare : {fare}");
         }
     }
 }
